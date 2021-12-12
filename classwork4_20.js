c1fcc03d15aefa1ba215a48b077d94afc2a979ba
GEVORG KEROBYAN <kerobyangevorg@gmail.com>
2021-12-12 22:42:53 +0400

    Temporary commented on 20th assignment

--- classwork4_20.js
+++ classwork4_20.js
@@ -7,30 +7,30 @@
 
 
 
-let x = 0;
-let g;
+// let x = 0;
+// let g;
 
-let arr = [];
-// let output = 0;
+// let arr = [];
+// // let output = 0;
 
-let param1 = x;
-let param2 = x+g;
+// let param1 = x;
+// let param2 = x+g;
 
-for (let i = 1; i <= 15; i++){
-   arr.push(i);
-}
+// for (let i = 1; i <= 15; i++){
+//    arr.push(i);
+// }
 
-let arrG = [];
+// let arrG = [];
 
-for (g = 4; g >= 0; g--)  {
+// for (g = 4; g >= 0; g--)  {
 
-    arrG.push(arr[x+g])
+//     arrG.push(arr[x+g])
 
-    x += 1;
-}
+//     x += 1;
+// }
 
-console.log(arrG);
-console.log(arr);
+// console.log(arrG);
+// console.log(arr);
 
 // arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]; 
 
